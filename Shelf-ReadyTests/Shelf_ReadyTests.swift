b7fd13ebca837b1e690193b16040d98a7223835a
Michael Fluharty <michael@fluharty.com>
2026-06-07 08:53:38 -0500

    Initial Commit

---
+++ Shelf-ReadyTests/Shelf_ReadyTests.swift
@@ -0,0 +1,18 @@
+//
+//  Shelf_ReadyTests.swift
+//  Shelf-ReadyTests
+//
+//  Created by Michael Fluharty on 6/7/26.
+//
+
+import Testing
+
+struct Shelf_ReadyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
